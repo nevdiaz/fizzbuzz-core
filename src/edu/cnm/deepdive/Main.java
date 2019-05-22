3da77caf130990e9c71d064fe94613c6e22eb456
nevdiaz <jenndiazz@icloud.com>
2019-05-22 14:26:48 -0600

    added java comments

--- src/edu/cnm/deepdive/Main.java
+++ src/edu/cnm/deepdive/Main.java
@@ -1,6 +1,18 @@
 package edu.cnm.deepdive;
 
+/**
+ * Exercises (as a simple console application) the methods of the {@link FizzBuzz} class.
+ * @author JD &amp; Deep Dive Coding Java + Android Cohort 7
+ * @version 1.0.0
+ */
 public class Main {
+
+  /**
+   * Computes and displays the fizz buzz values corresponding to the input values 1&ndash;100.
+   *
+   * @param args Command line arguments (ignored).
+   */
+
 
   public static void main(String[] args) {
     for( int i=1; i <=100; i++){
