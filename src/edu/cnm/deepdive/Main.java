20c8ab6710bfd264d9a9ef8d738c1cea7f25c298
nevdiaz <jenndiazz@icloud.com>
2019-05-22 09:46:20 -0600

    Added main and fixed bugs on test.

---
+++ src/edu/cnm/deepdive/Main.java
@@ -0,0 +1,11 @@
+package edu.cnm.deepdive;
+
+public class Main {
+
+  public static void main(String[] args) {
+    for( int i=1; i <=100; i++){
+      System.out.println(FizzBuzz.fizzbuzzValue(i));
+    }
+  }
+
+}
